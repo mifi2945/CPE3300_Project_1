bec67094e3851eeef80dbab4764d0ee4ba7b50af
filippovm <filippovm@msoe.edu>
2026-02-06 10:59:48 -0600

    pins changed

--- Src/protocol.c
+++ Src/protocol.c
@@ -93,10 +93,10 @@ void transmit(uint8_t length, char* message) {
 
 static void set_state(enum Rx_State state) {
 	// set pins
-	// PB5 = IDLE, PB6 = BUSY, PB7 = COLISSION
-	gpiob->BSRR = 1 << (5 + (state==IDLE ? 0 : 16));
-	gpiob->BSRR = 1 << (6 + (state==BUSY ? 0 : 16));
-	gpiob->BSRR = 1 << (7 + (state==COLLISION ? 0 : 16));
+	// PB0 = IDLE, PB1 = BUSY, PB2 = COLISSION
+	gpiob->BSRR = 1 << (0 + (state==IDLE ? 0 : 16));
+	gpiob->BSRR = 1 << (1 + (state==BUSY ? 0 : 16));
+	gpiob->BSRR = 1 << (2 + (state==COLLISION ? 0 : 16));
 
 	// set state variable
 	curr_state = state;
